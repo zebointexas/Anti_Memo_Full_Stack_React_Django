a360b875dfceff220d2361abbbdeaa9cc64b66ab
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2024-11-24 18:15:01 -0800

    Able to run backend + frontend, but cannot submit created list!!!

--- backend_Demo_Test/api/serializers.py
+++ backend_Demo_Test/api/serializers.py
@@ -2,18 +2,21 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
 
-class UserSerializer(serializers.ModelSerializer): 
-    class Meta: 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {"password":{"write_only":True}}
+        extra_kwargs = {"password": {"write_only": True}}
 
-    def create(self, validated_data): 
+    def create(self, validated_data):
+        print(validated_data)
         user = User.objects.create_user(**validated_data)
-        return user 
-    
-class NoteSerializer(serializers.ModelSerializer): 
-    class Meta: 
+        return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Note
         fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
